23aaf5c24211ab80e29bfd2edb644f5a5cf0bc56
phducdung04 <phamducdung147@gmail.com>
2024-07-09 08:28:44 +0700

    pull

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,16 @@
 const adminRoutes = require("./adminRoutes");
 const productRoutes = require("./productRoutes");
+const homeRoutes = require("./homeRoutes");
+const newsRoutes = require("./newsRoutes");
+const contactRoutes = require("./contactRoutes");
+
+
 const initWebRoutes = (app) => {
-      
- 
 
-
-
+    app.use("/", homeRoutes);
     app.use("/admin", adminRoutes);
+    app.use("/news", newsRoutes);
+    app.use("/contact", contactRoutes);
     app.use("/product", productRoutes);
 };
 
